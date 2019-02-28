a973c94417222f9ddf1ccaa25afe59c0a284e9ec
silky <nn880328@126.com>
2019-03-01 00:30:05 +0800

    提取红包，简易计算器，为了审核通过

--- app.js
+++ app.js
@@ -1,91 +1,65 @@
 //app.js
 App({
-	onLaunch: function() {
-		this.getOpenId().then((result) => {
-			//由于这里是网络请求，可能会在 Page.onLoad 之后才返回
-			// 所以此处加入 callback 以防止这种情况
-			if (this.userInfoCallback) {
-				this.userInfoCallback(result);
-			}
-		});
-	},
-	// 换取openId、ssesionKey
-	getOpenId() {
-		// 登录
-		return new Promise((resolve, reject) => {
-			wx.login({
-				success: res => {
-					// 发送 res.code 到后台换取 openId, sessionKey, unionId
-					this.ajax({
-						method: "GET",
-						url: "/xcx/api.php",
-						data: {
-							open: 1,
-							code: res.code
-						}
-					}).then((result) => {
-						wx.setStorageSync("openid", result.openid);
-						wx.setStorageSync("session_key", result.session_key);
-						resolve(result);
-					});
-				}
-			})
-		});
-	},
-	// 解密用户信息
-	encryptedData({ encryptedData, iv, Recommender = "" }) {
-		return this.ajax({
-			method: "POST",
-			url: "/xcx/uid/",
-			data: {
-				sessionKey: wx.getStorageSync("session_key"),
-				encryptedData,
-				iv,
-				Recommender
-			}
-		})
-	},
-	// 公共数据
-	globalData: {
-		userInfo: null,
-		url: 'https://x.daikan8.com',
-	},
-	// model:{method,url,data,config}
-	ajax(model) {
-		let headerConfig = { // 默认header ticket、token、params参数是每次请求需要携带的认证信息
-			ticket: '',
-			token: '',
-			params: '',
-			'content-type': 'application/x-www-form-urlencoded'
-		};
-		wx.showLoading({
-			title: '加载中',
-		})
-		// method默认
-		model.method = model.method || "POST";
-		//拼接url
-		model.url = this.globalData.url + model.url;
-		//返回Promise对象
-		return new Promise(function(resolve) {
-			wx.request({
-				method: model.method,
-				url: model.url,
-				data: model.data,
-				header: Object.assign({}, headerConfig, model.config), // 合并传递进来的配置
-				success: (res) => {
-					wx.hideLoading();
-					if (res.statusCode == 200) {
-						resolve(res.data);
-					} else {
-						//错误信息处理
-						wx.showModal({
-							title: '提示',
-							content: '服务器错误，请联系客服',
-							showCancel: false,
-						})
-					}
-				}
-			})
-		})
-	}
+  onLaunch: function() {
+    wx.login({
+      success: res => {
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+        this.ajax({
+          method: "GET",
+          url: "/xcx/api.php",
+          data: {
+            open: 1,
+            code: res.code
+          }
+        }).then((result) => {
+          wx.setStorageSync("openid", result.openid);
+          wx.setStorageSync("session_key", result.session_key);
+          wx.setStorageSync("unionId", result.unionid);
+        });
+      }
+    })
+  },
+  // 公共数据
+  globalData: {
+    userInfo: null,
+    url: 'https://x.daikan8.com',
+  },
+  // model:{method,url,data,config}
+  ajax(model) {
+    let headerConfig = { // 默认header ticket、token、params参数是每次请求需要携带的认证信息
+      ticket: '',
+      token: '',
+      params: '',
+      'content-type': 'application/x-www-form-urlencoded'
+    };
+    wx.showLoading({
+      title: '加载中',
+    })
+    // method默认
+    model.method = model.method || "POST";
+    //拼接url
+    model.url = this.globalData.url + model.url;
+    //返回Promise对象
+    return new Promise(function(resolve) {
+      wx.request({
+        method: model.method,
+        url: model.url,
+        data: model.data,
+        header: Object.assign({}, headerConfig, model.config), // 合并传递进来的配置
+        success: (res) => {
+          wx.hideLoading();
+          if (res.statusCode == 200) {
+            resolve(res.data);
+          } else {
+            //错误信息处理
+            wx.showModal({
+              title: '提示',
+              content: '服务器错误，请联系客服',
+              showCancel: false,
+            })
+          }
+        }
+      })
+    })
+  }
 })
--- pages/payback/index.js
+++ pages/payback/index.js
@@ -1,66 +1,59 @@
-// pages/index/index.js
+// pages/payback/index.js
+const app = getApp();
 Page({
 
    /**
     * 页面的初始数据
     */
    data: {
+      money: "",
       authModalShow: '', // 是否显示授权modal
    },
-   // 获取用户授权
-   authHandle(e) {
-      let data = e.detail;
-      data.Recommender = this.data.recommender;
-      app.encryptedData(data)
-         .then((result) => {
-            wx.setStorageSync("unionId", result);
-            this.setData({
-               authModalShow: false
-            });
-         });
+   // 获取目前提前金额
+   showMoney() {
+      app.ajax({
+         method: "GET",
+         url: "/xcx/uid/",
+         data: {
+            hqhb: wx.getStorageSync("openid")
+         }
+      }).then((result) => {
+         result = result >= 0 ? result : 0;
+         this.setData({
+            money: result
+         })
+      });
+   },
+   // 提现红包
+   getMoney() {
+      app.ajax({
+         method: "GET",
+         url: "/xcx/uid/",
+         data: {
+            tx: wx.getStorageSync("openid")
+         }
+      }).then((result) => {
+         if (result) {
+            wx.showModal({
+               title: '通知',
+               content: '提现成功！',
+               showCancel: false
+            })
+         } else {
+            wx.showModal({
+               title: '通知',
+               content: '提现失败！',
+               showCancel: false
+            })
+         }
+      });
    },
    /**
     * 生命周期函数--监听页面加载
     */
    onLoad: function(options) {
-      // 监测用户授权
-      wx.getSetting({
-         success: res => {
-            if (!res.authSetting['scope.userInfo']) {
-               this.setData({
-                  authModalShow: true
-               });
-               return false;
-            }
-            if (res.authSetting['scope.userInfo']) {
-               // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-               wx.getUserInfo({
-                  success: res => {
-                     // 可以将 res 发送给后台解码出 unionId
-                     res.Recommender = this.data.recommender;
-                     let sessionKey = wx.getStorageSync("session_key");
-                     if (!sessionKey) {
-                        app.userInfoCallback = result => {
-                           if (result) {
-                              wx.setStorageSync("session_key", result.session_key);
-                              app.encryptedData(res).then((result) => {
-                                 wx.setStorageSync("unionId", result);
-
-                              });
-                           }
-                        }
-                        return false;
-                     }
-                     app.encryptedData(res).then((result) => {
-                        wx.setStorageSync("unionId", result);
-
-                     });
-
-                  }
-               })
-            }
-         }
-      });
+      // 获取目前提前金额
+      this.showMoney();
    },
 
    /**
