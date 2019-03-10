a02d1d12543a7765459367a918a5405b865c9a59
silky <nn880328@126.com>
2019-03-10 23:58:00 +0800

    微信天气

--- utils/util.js
+++ utils/util.js
@@ -1,5 +1,3 @@
-// 工具函数
-
 function formatTime(date) {
   var year = date.getFullYear()
   var month = date.getMonth() + 1
@@ -7,10 +5,10 @@ function formatTime(date) {
 
   var hour = date.getHours()
   var minute = date.getMinutes()
-  var second = date.getSeconds();
+  var second = date.getSeconds()
 
 
-  return [year, month, day].map(formatNumber).join('.') + ' ' + [hour, minute, second].map(formatNumber).join(':')
+  return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
 }
 
 function formatNumber(n) {
@@ -18,23 +16,8 @@ function formatNumber(n) {
   return n[1] ? n : '0' + n
 }
 
-// 将一维数组转为二维数组
-function listToMatrix(list, elementPerSubArray) {
-  let matrix = [], i, k;
 
-  for (i = 0, k = -1; i < list.length; i += 1) {
-    if (i % elementPerSubArray === 0) {
-      k += 1;
-      matrix[k] = [];
-    }
-
-    matrix[k].push(list[i]);
-  }
-
-  return matrix;
-}
 
 module.exports = {
-  formatTime: formatTime,
-  listToMatrix: listToMatrix,
+  formatTime: formatTime
 }
